ab684b861990bcb509d4cdd0b912df1cf30199c0
ilham <ilham.puji100@gmail.com>
2019-06-20 20:44:27 +0700

    Set Buyer Middleware

--- app/Http/Middleware/Auth/BuyerMiddleware.php
+++ app/Http/Middleware/Auth/BuyerMiddleware.php
@@ -3,18 +3,23 @@
 namespace App\Http\Middleware\Auth;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class BuyerMiddleware
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if (Auth::guest()) {
+            return redirect()->guest(route('landing'))
+                ->with('error_login', 'Proses yang Anda minta memerlukan otentikasi, silahkan masuk ke akun Anda.');
+        }
+        return $next($request); //lanjur ke prosess berikutnya
     }
 }
