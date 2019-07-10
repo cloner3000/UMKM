5ff4d0f73f5c071c1c87a54e7b60a61aa74f3a2e
ilham <ilham.puji100@gmail.com>
2019-07-10 17:29:08 +0700

    Minor Update model Review

--- app/Model/Review.php
+++ app/Model/Review.php
@@ -9,7 +9,7 @@ class Review extends Model
     protected $table = 'reviews';
     protected $guarded = ['id'];
 
-    public function getProduct()
+    public function getCart()
     {
         return $this->belongsTo('App\Model\Cart','carts_id');
     }
